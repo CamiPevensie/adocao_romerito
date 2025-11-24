c1fdfc7dd74b732272a36a57d664b3ee1865eb5a
Filipe <fmarcelino154@gmail.com>
2025-11-24 08:25:17 -0300

    Co-authored-by: Camila <CamiGamgi@users.noreply.github.com>

--- adot/app.py
+++ adot/app.py
@@ -13,10 +13,44 @@ Sessao_base = sessionmaker(engine)
 def index():
     return render_template('index.html')
 
+@app.route('/adocao')
+def adocao():
+    return render_template('adocao.html')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
-@app.route('/cadastro')
+@app.route('/mascotes')
+def mascotes():
+    return render_template('mascotes.html')
+
+@app.route('/cadastro', methods = ['GET','POST'])
 def cadastro():
+    if request.method == 'POST':
+        nome = request.form['nome_completo_form']
+        nome_completo = request.form['nome_form']
+        telefone = request.form['telefone_form']
+        email = request.form['email_form']
+        senha = request.form['senha_form']
+        rua = request.form['rua_form']
+        bairro = request.form['bairro_form']
+        cidade = request.form['cidade_form']
+        numero = request.form['numero_form']
+        complemento = request.form['complemento_form']
+        estado = request.form['estado_form']
+
+        usuario = Usuario(nome=nome, nome_completo=nome_completo, telefone=telefone, email=email, senha=senha, rua=rua, bairro=bairro, cidade=cidade, numero=numero, complemento=complemento, estado=estado)
+        with Sessao_base as sessao:
+            sessao.add(usuario)
+            sessao.commit()
+        return redirect(url_for('index'))
     return render_template('cadastro.html')
+
+@app.route('/cadastrar_animal')
+def cadastrar_animal():
+    return render_template()
