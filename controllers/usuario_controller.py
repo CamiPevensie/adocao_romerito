4abdaad41fde1902c55dfe220d47e6a21e04fed4
Filipe <fmarcelino154@gmail.com>
2025-12-02 18:43:36 -0300

    Início do perfil.py

--- controllers/usuario_controller.py
+++ controllers/usuario_controller.py
@@ -1,12 +1,17 @@
-from flask import Blueprint, render_template, url_for, request, redirect
+from flask import Blueprint, render_template, url_for, request, redirect, flash, session
 from database import Sessao_base
 from models.usuario import Usuario
 
 usuario_bp = Blueprint('usuario',__name__)
 
-@usuario_bp.route('/perfil', methods=['GET,''POST'])
-def perfil():
+@usuario_bp.route('/perfil/<int: usuario_id>', methods=['GET,''POST'])
+def perfil(animal_id):
+    # Se o usuário não estiver logado
+    if 'usuario_id' not in session:
+        flash("Faça login para continuar.", "warning")
+        return redirect(url_for('auth.login'))
+
 
     
     with Sessao_base() as sessao:
-        perfil = sessao.query('Usuario').one()
+        perfil = sessao.query(Usuario).filter(Usuario.id == animal_id).first()
